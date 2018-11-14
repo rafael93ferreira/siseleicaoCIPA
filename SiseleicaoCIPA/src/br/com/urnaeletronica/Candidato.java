989ac2e1ef77014ace61e938586d975dde63488f
26142858 <26142858@UNA009-L103-015.serlab.intra>
2018-11-13 21:31:22 -0300

    Classes Candidato, Voto

---
+++ SiseleicaoCIPA/src/br/com/urnaeletronica/Candidato.java
@@ -0,0 +1,17 @@
+package br.com.urnaeletronica;
+
+public class Candidato {
+	
+	private String nome;
+	private int numero;
+	
+	public String getCandidatoById(int id)
+	{
+		return "AAAAA";
+	}
+	
+	public void listarCandidatos()
+	{
+		
+	}
+}
---
+++ SiseleicaoCIPA/src/br/com/urnaeletronica/Voto.java
@@ -0,0 +1,15 @@
+package br.com.urnaeletronica;
+
+public class Voto {
+
+	public void registrarVoto()
+	{
+		
+	}
+	
+	public void apurarVotacao()
+	{
+		
+	}
+	
+}
