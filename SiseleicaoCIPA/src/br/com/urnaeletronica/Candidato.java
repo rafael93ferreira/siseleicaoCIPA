c0003321cd6c48e94b4cc49de154eb94b14a4552
WalterPaes <wpmj1277@gmail.com>
2018-11-19 09:57:31 -0300

    Criando a Eleicao

--- SiseleicaoCIPA/src/br/com/urnaeletronica/Candidato.java
+++ SiseleicaoCIPA/src/br/com/urnaeletronica/Candidato.java
@@ -2,8 +2,9 @@ package br.com.urnaeletronica;
 
 public class Candidato {
 	
-	private String nome;
+	private String nome, foto;
 	private int numero;
+	
 	
 	public String getCandidatoById(int id)
 	{
@@ -13,5 +14,36 @@ public class Candidato {
 	public void listarCandidatos()
 	{
 		
+	}
+	
+	// Getters and Setters
+	public void setNome(String nome)
+	{
+		this.nome = nome;
+	}
+	
+	public String getNome()
+	{
+		return this.nome;
+	}
+	
+	public void setFoto(String foto)
+	{
+		this.foto = foto;
+	}
+	
+	public String getFoto()
+	{
+		return this.foto;
+	}
+	
+	public void setNumero(int numero)
+	{
+		this.numero = numero;
+	}
+	
+	public int getNumero()
+	{
+		return this.numero;
 	}
 }
